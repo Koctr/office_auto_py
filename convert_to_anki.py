1f86a5c9e6efbd6086fa2cf8e32a855fba969839
Koctr <koctr@china.com>
2019-06-14 17:38:17 +0800

    convert txt to anki file

--- convert_to_anki.py
+++ convert_to_anki.py
@@ -2,45 +2,32 @@
 # author = 'K0ctr'
 
 
-def format_excel_questions(fullname):
-    """
+import os
 
-    :param filename:
+"""
+将原始文本转换为anki格式的文本
+"""
+
+
+def convert_to_anki1(init_file_name,tag):
+    """
+    针对以下情况：
+    1、每道题都是数字开头
+    2、每道题的答案前有“答案”两字
+    3、答案总是在单独一行
+    :param init_file_name: 原始文件的文件名，需要完整路径
+    :param tag: anki中的标签名
     :return:
     """
-    pass
+    with open(init_file_name,"r",encoding="utf-8") as conv_file:
+        with open("anki.txt","w",encoding="utf-8") as anki:
+            for line in conv_file:
+                if line.strip().startswith("答案"):
+                    line = "\t" + line.strip()+"\t"+tag+os.linesep
+                    print(line)
+                    anki.write(line)
+                else:
+                    anki.write(line.strip())
 
+convert_to_anki1("test.txt","TEST")
 
-with open(r"D:\网络安全合规指引.txt", "r", encoding="utf-8") as f1:
-    with open(r"网络安全合规指引.txt", "w", encoding="utf-8") as f2:
-        for line in f1:
-            """
-            if line.startswith("答案"):
-                line = "\t" + line
-            """
-            if line[0].isdigit() or line[0:2].isdigit() or line[0:3].isdigit():
-                line = "\n" + line.rstrip("\r\n")
-            else:
-                line = line.rstrip("\r\n")
-            f2.write(line)
-with open(r"网络安全合规指引.txt", 'r', encoding="utf-8") as f1:
-    with open(r'网络安全合规指引.xls', "w", encoding="utf-8") as f2:
-        for line in f1:
-            f2.write(line.strip() + "\t" + "网络安全合规指引" + "\n")
-
-with open(r"D:\信息安全管理实践.txt", 'r', encoding="utf-8") as f1:
-    with open(r"信息安全管理实践.txt", 'w', encoding="utf-8") as f2:
-        for line in f1:
-            """
-            if line.startswith("答案"):
-                line = "\t" + line
-            """
-            if line[0].isdigit() or line[0:2].isdigit() or line[0:3].isdigit():
-                line = "\n" + line.rstrip("\r\n")
-            else:
-                line = line.rstrip("\r\n")
-            f2.write(line)
-with open(r"信息安全管理实践.txt", 'r', encoding="utf-8") as f1:
-    with open(r'信息安全管理实践.xls', "w", encoding="utf-8") as f2:
-        for line in f1:
-            f2.write(line.strip() + "\t" + "信息安全管理实践" + "\n")
